167fcd083026803329a6086f063b922161d3c669
ThiwankaReiss <thiwankar2003@gmail.com>
2026-09-15 09:03:31 +0530

    debugingtorque value issues

---
+++ SimscapeTest4/Unit_validation/Simscape_val_mm/LowerLinkAssmbly_v2_DataFile.m
@@ -0,0 +1,285 @@
+% Simscape(TM) Multibody(TM) version: 23.2
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:lower_link_v2
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+lower_link_v2.RigidTransform(20).translation = [0.0 0.0 0.0];
+lower_link_v2.RigidTransform(20).angle = 0.0;
+lower_link_v2.RigidTransform(20).axis = [0.0 0.0 0.0];
+lower_link_v2.RigidTransform(20).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(1).translation = [0 23.5 0];  % mm
+lower_link_v2.RigidTransform(1).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(1).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(1).ID = "B[Worm Drive Motor JGY370-1:-:MotorCoverTop-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(2).translation = [18.000000000000007 33.000000000000085 -25.200000000000003];  % mm
+lower_link_v2.RigidTransform(2).angle = 3.1415926535897931;  % rad
+lower_link_v2.RigidTransform(2).axis = [1 0 0];
+lower_link_v2.RigidTransform(2).ID = "F[Worm Drive Motor JGY370-1:-:MotorCoverTop-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(3).translation = [-108.00000000000001 -1.5051204082357215 26.000000000000004];  % mm
+lower_link_v2.RigidTransform(3).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(3).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+lower_link_v2.RigidTransform(3).ID = "B[lowerLink_v2-1:-:Worm Drive Motor JGY370-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(4).translation = [-3.800000000002882 11 -32.999999999999787];  % mm
+lower_link_v2.RigidTransform(4).angle = 0;  % rad
+lower_link_v2.RigidTransform(4).axis = [0 0 0];
+lower_link_v2.RigidTransform(4).ID = "F[lowerLink_v2-1:-:Worm Drive Motor JGY370-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(5).translation = [0 23.499999999999993 0];  % mm
+lower_link_v2.RigidTransform(5).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(5).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(5).ID = "B[Worm Drive Motor JGY370-2:-:MotorCoverTop-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(6).translation = [17.999999999999972 32.999999999999986 -25.200000000000003];  % mm
+lower_link_v2.RigidTransform(6).angle = 3.1415926535897931;  % rad
+lower_link_v2.RigidTransform(6).axis = [-1 5.150362813701729e-33 -7.4395165567072828e-17];
+lower_link_v2.RigidTransform(6).ID = "F[Worm Drive Motor JGY370-2:-:MotorCoverTop-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(7).translation = [108.00000000000001 0 -26.000000000000004];  % mm
+lower_link_v2.RigidTransform(7).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(7).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+lower_link_v2.RigidTransform(7).ID = "B[lowerLink_v2-1:-:Worm Drive Motor JGY370-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(8).translation = [-3.8000000000000078 11.000000000000002 -33.000000000000007];  % mm
+lower_link_v2.RigidTransform(8).angle = 5.5511151231257827e-17;  % rad
+lower_link_v2.RigidTransform(8).axis = [5.5511151231257827e-17 1 1.5407439555097887e-33];
+lower_link_v2.RigidTransform(8).ID = "F[lowerLink_v2-1:-:Worm Drive Motor JGY370-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(9).translation = [-143 15.694879591761342 13.299999999999999];  % mm
+lower_link_v2.RigidTransform(9).angle = 0;  % rad
+lower_link_v2.RigidTransform(9).axis = [0 0 0];
+lower_link_v2.RigidTransform(9).ID = "B[lowerLink_v2-1:-:MotorCoverTop-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(10).translation = [-2.9999999999999645 35.000000000000071 0];  % mm
+lower_link_v2.RigidTransform(10).angle = 3.1415926535897931;  % rad
+lower_link_v2.RigidTransform(10).axis = [0.70710678118655101 -0.70710678118654413 0];
+lower_link_v2.RigidTransform(10).ID = "F[lowerLink_v2-1:-:MotorCoverTop-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(11).translation = [143.00000000000003 17.199999999999978 -13.299999999999986];  % mm
+lower_link_v2.RigidTransform(11).angle = 3.1415926535897931;  % rad
+lower_link_v2.RigidTransform(11).axis = [-1 -0 -0];
+lower_link_v2.RigidTransform(11).ID = "B[lowerLink_v2-1:-:MotorCoverTop-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(12).translation = [-3.0000000000000071 34.999999999999986 -3.0387789003455829e-15];  % mm
+lower_link_v2.RigidTransform(12).angle = 3.1415926535897931;  % rad
+lower_link_v2.RigidTransform(12).axis = [-0.70710678118654757 -0.70710678118654746 -8.1837678465469459e-17];
+lower_link_v2.RigidTransform(12).ID = "F[lowerLink_v2-1:-:MotorCoverTop-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(13).translation = [-22 -20.000000000000004 -41.001000000000005];  % mm
+lower_link_v2.RigidTransform(13).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(13).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(13).ID = "B[lowerLink_v2-1:-:DummyBattery-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(14).translation = [41.001000000000161 -16.000000000000007 -41.999999999999851];  % mm
+lower_link_v2.RigidTransform(14).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(14).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(14).ID = "F[lowerLink_v2-1:-:DummyBattery-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(15).translation = [0 0 0];  % mm
+lower_link_v2.RigidTransform(15).angle = 0;  % rad
+lower_link_v2.RigidTransform(15).axis = [0 0 0];
+lower_link_v2.RigidTransform(15).ID = "B[lowerLink_v2-1:-:]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(16).translation = [0 0 0];  % mm
+lower_link_v2.RigidTransform(16).angle = 0;  % rad
+lower_link_v2.RigidTransform(16).axis = [0 0 0];
+lower_link_v2.RigidTransform(16).ID = "F[lowerLink_v2-1:-:]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(17).translation = [-22 -20.000000000000004 -41.001000000000005];  % mm
+lower_link_v2.RigidTransform(17).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(17).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(17).ID = "B[lowerLink_v2-1:-:DummyBuckConverter-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(18).translation = [-41.001000000000232 16.000000000000281 -91.999999999999858];  % mm
+lower_link_v2.RigidTransform(18).angle = 2.0943951023931913;  % rad
+lower_link_v2.RigidTransform(18).axis = [0.5773502691896244 -0.5773502691896284 0.57735026918962451];
+lower_link_v2.RigidTransform(18).ID = "F[lowerLink_v2-1:-:DummyBuckConverter-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(19).translation = [-22 -20.000000000000004 -41.001000000000005];  % mm
+lower_link_v2.RigidTransform(19).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(19).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(19).ID = "B[lowerLink_v2-1:-:DummyBuckConverter-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+lower_link_v2.RigidTransform(20).translation = [41.000999999999976 -16 8.0000000000001563];  % mm
+lower_link_v2.RigidTransform(20).angle = 2.0943951023931953;  % rad
+lower_link_v2.RigidTransform(20).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+lower_link_v2.RigidTransform(20).ID = "F[lowerLink_v2-1:-:DummyBuckConverter-2]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+lower_link_v2.Solid(5).mass = 0.0;
+lower_link_v2.Solid(5).CoM = [0.0 0.0 0.0];
+lower_link_v2.Solid(5).MoI = [0.0 0.0 0.0];
+lower_link_v2.Solid(5).PoI = [0.0 0.0 0.0];
+lower_link_v2.Solid(5).color = [0.0 0.0 0.0];
+lower_link_v2.Solid(5).opacity = 0.0;
+lower_link_v2.Solid(5).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+lower_link_v2.Solid(1).mass = 0.20000000000000001;  % kg
+lower_link_v2.Solid(1).CoM = [-1.1539918768186805 11.069032432102826 -19.477373881993461];  % mm
+lower_link_v2.Solid(1).MoI = [98.155384137076609 104.30158856210629 22.36756347901207];  % kg*mm^2
+lower_link_v2.Solid(1).PoI = [-0.31455682296466442 -6.1400941408117298 -0.015856377872091938];  % kg*mm^2
+lower_link_v2.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+lower_link_v2.Solid(1).opacity = 1;
+lower_link_v2.Solid(1).ID = "Worm Drive Motor JGY370*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+lower_link_v2.Solid(2).mass = 0.094500967989907619;  % kg
+lower_link_v2.Solid(2).CoM = [0 -4.7259127573096507 -0.045585740656684436];  % mm
+lower_link_v2.Solid(2).MoI = [78.742090712921041 441.26170300257746 424.53120587071487];  % kg*mm^2
+lower_link_v2.Solid(2).PoI = [0.33299209671726032 50.178052451416079 -0.76750682282196547];  % kg*mm^2
+lower_link_v2.Solid(2).color = [0.59019607843137256 0.57098039215686269 0.52705882352941169];
+lower_link_v2.Solid(2).opacity = 1;
+lower_link_v2.Solid(2).ID = "lowerLink_v2*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+lower_link_v2.Solid(3).mass = 0.011430225692313896;  % kg
+lower_link_v2.Solid(3).CoM = [18.318466152636656 28.1614745392211 -16.839353660718871];  % mm
+lower_link_v2.Solid(3).MoI = [3.945216781416788 3.1828473446409 5.2850726323199773];  % kg*mm^2
+lower_link_v2.Solid(3).PoI = [-0.17789751962325637 -0.015431840834528697 0.098871569929201111];  % kg*mm^2
+lower_link_v2.Solid(3).color = [0.45019607843137249 0.43098039215686273 0.41999999999999998];
+lower_link_v2.Solid(3).opacity = 1;
+lower_link_v2.Solid(3).ID = "MotorCoverTop*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+lower_link_v2.Solid(4).mass = 0.23999999999999999;  % kg
+lower_link_v2.Solid(4).CoM = [0 0 -20];  % mm
+lower_link_v2.Solid(4).MoI = [52.480000000000011 135.68000000000004 124.16000000000003];  % kg*mm^2
+lower_link_v2.Solid(4).PoI = [0 0 0];  % kg*mm^2
+lower_link_v2.Solid(4).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+lower_link_v2.Solid(4).opacity = 1;
+lower_link_v2.Solid(4).ID = "DummyBattery*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+lower_link_v2.Solid(5).mass = 0.080000000000000002;  % kg
+lower_link_v2.Solid(5).CoM = [0 0 -20];  % mm
+lower_link_v2.Solid(5).MoI = [17.493333333333336 45.226666666666674 41.386666666666677];  % kg*mm^2
+lower_link_v2.Solid(5).PoI = [0 0 0];  % kg*mm^2
+lower_link_v2.Solid(5).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+lower_link_v2.Solid(5).opacity = 1;
+lower_link_v2.Solid(5).ID = "DummyBuckConverter*:*Default";
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the CylindricalJoint structure array by filling in null values.
+lower_link_v2.CylindricalJoint(4).Rz.Pos = 0.0;
+lower_link_v2.CylindricalJoint(4).Pz.Pos = 0.0;
+lower_link_v2.CylindricalJoint(4).ID = "";
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+lower_link_v2.CylindricalJoint(1).Rz.Pos = 90.000000000000554;  % deg
+lower_link_v2.CylindricalJoint(1).Pz.Pos = 0;  % mm
+lower_link_v2.CylindricalJoint(1).ID = "[Worm Drive Motor JGY370-1:-:MotorCoverTop-1]";
+
+lower_link_v2.CylindricalJoint(2).Rz.Pos = 179.99999999999997;  % deg
+lower_link_v2.CylindricalJoint(2).Pz.Pos = 0;  % mm
+lower_link_v2.CylindricalJoint(2).ID = "[lowerLink_v2-1:-:Worm Drive Motor JGY370-1]";
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+lower_link_v2.CylindricalJoint(3).Rz.Pos = 89.999999999999986;  % deg
+lower_link_v2.CylindricalJoint(3).Pz.Pos = 0;  % mm
+lower_link_v2.CylindricalJoint(3).ID = "[Worm Drive Motor JGY370-2:-:MotorCoverTop-2]";
+
+lower_link_v2.CylindricalJoint(4).Rz.Pos = -179.99999999999997;  % deg
+lower_link_v2.CylindricalJoint(4).Pz.Pos = 0;  % mm
+lower_link_v2.CylindricalJoint(4).ID = "[lowerLink_v2-1:-:Worm Drive Motor JGY370-2]";
+
+
+%Initialize the PlanarJoint structure array by filling in null values.
+lower_link_v2.PlanarJoint(3).Rz.Pos = 0.0;
+lower_link_v2.PlanarJoint(3).Px.Pos = 0.0;
+lower_link_v2.PlanarJoint(3).Py.Pos = 0.0;
+lower_link_v2.PlanarJoint(3).ID = "";
+
+lower_link_v2.PlanarJoint(1).Rz.Pos = 90.000000000000199;  % deg
+lower_link_v2.PlanarJoint(1).Px.Pos = 0;  % mm
+lower_link_v2.PlanarJoint(1).Py.Pos = 0;  % mm
+lower_link_v2.PlanarJoint(1).ID = "[lowerLink_v2-1:-:DummyBattery-1]";
+
+lower_link_v2.PlanarJoint(2).Rz.Pos = 90.000000000000199;  % deg
+lower_link_v2.PlanarJoint(2).Px.Pos = 0;  % mm
+lower_link_v2.PlanarJoint(2).Py.Pos = 0;  % mm
+lower_link_v2.PlanarJoint(2).ID = "[lowerLink_v2-1:-:DummyBuckConverter-1]";
+
+lower_link_v2.PlanarJoint(3).Rz.Pos = 90.000000000000199;  % deg
+lower_link_v2.PlanarJoint(3).Px.Pos = 0;  % mm
+lower_link_v2.PlanarJoint(3).Py.Pos = 0;  % mm
+lower_link_v2.PlanarJoint(3).ID = "[lowerLink_v2-1:-:DummyBuckConverter-2]";
+
+
+%Initialize the PrismaticJoint structure array by filling in null values.
+lower_link_v2.PrismaticJoint(2).Pz.Pos = 0.0;
+lower_link_v2.PrismaticJoint(2).ID = "";
+
+lower_link_v2.PrismaticJoint(1).Pz.Pos = 0;  % m
+lower_link_v2.PrismaticJoint(1).ID = "[lowerLink_v2-1:-:MotorCoverTop-1]";
+
+lower_link_v2.PrismaticJoint(2).Pz.Pos = 0;  % m
+lower_link_v2.PrismaticJoint(2).ID = "[lowerLink_v2-1:-:MotorCoverTop-2]";
+
---
+++ SimscapeTest4/program.m
@@ -0,0 +1,19 @@
+clc;
+clear;
+close all
+
+Arm_DataFile;
+Gripper_v2_ASSembly_DataFile;
+LowerLinkAssmbly_v2_DataFile;
+x30degreeinclineAssembly_DataFile;
+Actuactor_bottom_DataFile;
+
+
+% Set model name (replace with your actual .slx file name without extension)
+%modelName = 'main';
+
+% Load the model into memory
+%load_system(modelName);
+
+% Run the simulation
+%simResults = sim(modelName);
